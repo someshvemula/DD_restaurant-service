e153d8b942e9f3bbf1090c8d48cd721b9bb0c490
Somesh Vemula <ratansomesh@gmail.com>
2023-09-02 20:57:21 -0400

    Change the data type of attribute fieldValue from long to String in ResourceAlreadyExistsException.

--- src/main/java/com/dishdash/restaurantservice/exception/ResourceAlreadyExistsException.java
+++ src/main/java/com/dishdash/restaurantservice/exception/ResourceAlreadyExistsException.java
@@ -8,9 +8,9 @@ public class ResourceAlreadyExistsException extends RuntimeException{
 
     private String resourceName;
     private String fieldName;
-    private long fieldValue;
+    private String fieldValue;
 
-    public ResourceAlreadyExistsException(String resourceName, String fieldName, long fieldValue) {
+    public ResourceAlreadyExistsException(String resourceName, String fieldName, String fieldValue) {
         super(String.format("%S already exists with %s: %s", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
