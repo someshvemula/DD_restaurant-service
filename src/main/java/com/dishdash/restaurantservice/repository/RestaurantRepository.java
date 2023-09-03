c0ebe2c8d5334cd868a9efcc147617388f6ee5b5
Somesh Vemula <ratansomesh@gmail.com>
2023-09-02 20:58:28 -0400

    Add findByWebsite method in the repository layer.

--- src/main/java/com/dishdash/restaurantservice/repository/RestaurantRepository.java
+++ src/main/java/com/dishdash/restaurantservice/repository/RestaurantRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     @Query("SELECT r FROM Restaurant r WHERE " +
             "(:cuisine IS NULL OR r.cuisine = :cuisine) " +
             "AND (:search IS NULL OR r.name LIKE %:search%)")
     List<Restaurant> findRestaurants(Cuisine cuisine, String search);
+
+    Optional<Restaurant> findByWebsite(String website);
 }
